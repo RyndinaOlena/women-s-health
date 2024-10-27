97a8b5d883c6572b0949ccffcb97cc9586c6e9f4
Ryndina Olena <110286532+RyndinaOlena@users.noreply.github.com>
2024-10-27 13:18:18 +0200

    initial

---
+++ women-s-health/src/App.js
@@ -0,0 +1,14 @@
+import '../src/styles/index.scss'
+import Bonus from './pages/ bonus';
+import Hero from './pages/hero';
+
+function App() {
+  return (
+    <div >
+      <Hero />
+      <Bonus />
+    </div>
+  );
+}
+
+export default App;
---
+++ women-s-health/src/pages/hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <p className='hero_text'>Старт 22.11</p>
+            <div className='hero_title'>
+                <h1><span className='checked_letter'>Н</span>аучись восстанавливать свое женское здоровье с помощью сил природы</h1>
+                <button>Цена 1111грн вместо 5800грн</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero
