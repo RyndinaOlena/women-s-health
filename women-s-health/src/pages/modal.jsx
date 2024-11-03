19caaca00cfbe21cb316324e48ce9f2bc7273d16
Ryndina Olena <110286532+RyndinaOlena@users.noreply.github.com>
2024-11-03 09:41:10 +0200

    women

--- women-s-health/src/pages/modal.jsx
+++ women-s-health/src/pages/modal.jsx
@@ -5,19 +5,15 @@ const Modal = ({ isOpen, onClose }) => {
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-
-                <a className='modal_content_text__link_pay' href='https://www.privat24.ua/send/d8wyl'><FaLink /> Нажми чтобы оплатить </a>
-                {/* <div className='qr_code_img'></div> */}
-
                 <div className='modal_content_text'>
-
+                    <a className='modal_content_text__link_pay' rel='nofollow' href='https://www.privat24.ua/send/d8wyl'><FaLink /> Нажми чтобы оплатить </a>
+                    <div className='qr_code_img'></div>
                     <button className="close-button" onClick={onClose}>
                         &times;
                     </button>
                     <p className='modal-text'>"После завершения оплаты напишите мне или сбросьте скрин"</p>
-                    <p className='modal-text'>"Оплата по реквизитам или напишите мне в личные сообщения для большей информации об оплате"</p>
-                    <p className='modal-text'>"Для завершения вашего заказа нажмите на кнопку выше. Вы будете перенаправлены на защищенный платежный сервис для безопасной оплаты."</p>
-
+                    {/* <p className='modal-text'>"Оплата по реквизитам или напишите мне в личные сообщения для большей информации об оплате"</p>
+                    <p className='modal-text'>"Для завершения вашего заказа нажмите на кнопку выше. Вы будете перенаправлены на защищенный платежный сервис для безопасной оплаты."</p> */}
                 </div>
 
             </div>
